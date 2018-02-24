4e8289e73fd7f90c2747ac1993083395b5404011
wakamsha <wakamsha@gmail.com>
2018-02-24 15:21:24 +0900

    style: code format

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,75 +1,76 @@
-import {Observable} from 'rxjs';
-import {run} from '@cycle/rxjs-run';
-import {DOMSource} from '@cycle/dom/rxjs-typings';
-import {div, input, hr, h1, p, VNode, makeDOMDriver, CycleDOMEvent, code} from '@cycle/dom';
-import {makeScrollDriver} from './drivers/makeScrollDriver';
+import { Observable } from 'rxjs';
+import { run } from '@cycle/rxjs-run';
+import { DOMSource } from '@cycle/dom/rxjs-typings';
+import { div, input, hr, h1, p, VNode, makeDOMDriver, CycleDOMEvent, code } from '@cycle/dom';
+import { makeScrollDriver } from './drivers/makeScrollDriver';
 import {
-    initialMainState, MainState, MainStateAction, makeUpdateNameAction,
-    makeUpdateOffsetTopAction
+    initialMainState,
+    MainState,
+    MainStateAction,
+    makeUpdateNameAction,
+    makeUpdateOffsetTopAction,
 } from './MainState';
 
 type SoAll = {
     DOM: DOMSource;
     Scroll: Observable<string>;
-}
+};
 
 type SiAll = {
     DOM: Observable<VNode>;
     Scroll: Observable<number>;
-}
+};
 
-function main({DOM, Scroll}: SoAll): SiAll {
-
+function main({ DOM, Scroll }: SoAll): SiAll {
     const inputName$: Observable<CycleDOMEvent> = DOM.select('.field').events('input');
     const inputScroll$: Observable<CycleDOMEvent> = DOM.select('.scrollable__input').events('input');
 
     const state: Observable<MainState> = Observable.merge(
         inputName$.map((ev: CycleDOMEvent) => makeUpdateNameAction((ev.ownerTarget as HTMLInputElement).value)),
-        inputScroll$.map((ev: CycleDOMEvent) => makeUpdateOffsetTopAction(Number((ev.ownerTarget as HTMLInputElement).value)))
-    ).scan((acc: MainState, action: MainStateAction) => action(acc), initialMainState).startWith(initialMainState);
+        inputScroll$.map((ev: CycleDOMEvent) =>
+            makeUpdateOffsetTopAction(Number((ev.ownerTarget as HTMLInputElement).value)),
+        ),
+    )
+        .scan((acc: MainState, action: MainStateAction) => action(acc), initialMainState)
+        .startWith(initialMainState);
 
-    const dom$ = Observable.combineLatest(
-        state,
-        Scroll.startWith('0px'),
-        (state, scroll) => {
-            return div('.container', [
-                div('.row', [
-                    div('.col-sm-8', [
-                        div('.well', [
-                            input('.field.form-control', {props: {
+    const dom$ = Observable.combineLatest(state, Scroll.startWith('0px'), (state, scroll) => {
+        return div('.container', [
+            div('.row', [
+                div('.col-sm-8', [
+                    div('.well', [
+                        input('.field.form-control', {
+                            props: {
                                 placeholder: '君の名は… ',
-                                value: state.name
-                            }}),
-                            hr(),
-                            h1([`お前は… ${state.name} ${state.name && 'だ！'}`]),
-                            div([
-                                'baseUrl: ',
-                                code([state.baseUrl])
-                            ])
-                        ])
-                    ])
+                                value: state.name,
+                            },
+                        }),
+                        hr(),
+                        h1([`お前は… ${state.name} ${state.name && 'だ！'}`]),
+                        div(['baseUrl: ', code([state.baseUrl])]),
+                    ]),
                 ]),
-                input('.scrollable__input.form-control', {
-                    props: {
-                        type: 'number',
-                        value: state.offsetTop
-                    }
-                }),
-                p('.scrollable__counter', [scroll]),
-                div('.scroll-content')
-            ]);
-        }
-    );
+            ]),
+            input('.scrollable__input.form-control', {
+                props: {
+                    type: 'number',
+                    value: state.offsetTop,
+                },
+            }),
+            p('.scrollable__counter', [scroll]),
+            div('.scroll-content'),
+        ]);
+    });
 
     return <SiAll>{
         DOM: dom$,
-        Scroll: inputScroll$.map((ev: CycleDOMEvent) => Number((ev.ownerTarget as HTMLInputElement).value))
+        Scroll: inputScroll$.map((ev: CycleDOMEvent) => Number((ev.ownerTarget as HTMLInputElement).value)),
     };
 }
 
 const drivers = {
     DOM: makeDOMDriver('#app'),
-    Scroll: makeScrollDriver({ element: document.body, duration: 600 })
+    Scroll: makeScrollDriver({ element: document.body, duration: 600 }),
 };
 
 run(main, drivers);
